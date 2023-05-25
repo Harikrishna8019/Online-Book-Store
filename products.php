f6f5833922a62785a16e90045750f24609870997
Harikrishna8019 <132816270+Harikrishna8019@users.noreply.github.com>
2023-05-25 10:54:54 +0530

    Add files via upload

---
+++ products.php
@@ -0,0 +1,133 @@
+
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</head>
+<style type="text/css">
+	body{
+		background-color: skyblue; 
+	}
+	h1{
+		text-align: center;
+		size: 20px;
+		color: black;
+
+	}
+	img {
+		margin-top: 10px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 180px;
+}
+a:link, a:visited {
+  background-color: green;
+  color: white;
+  padding: 14px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+
+
+div.gallery {
+margin: 5px;
+border: 1px solid grey;
+float: left;
+width: 280px;
+border-radius: 5px;
+padding: 20px;
+}
+
+div.gallery img {
+border-radius: 20 px;
+width: 100%;
+height: 200;
+}
+div.desc {
+padding: 15px;
+text-align: center;
+font-size: 20px;
+text-decoration: none;
+}
+</style>
+<body>
+	<img src="h9.avif" alt="logo">
+	<div class="container">
+	<h1>Welcome to Online Book Store</h1>
+	<hr>
+</div>
+<div class="gallery">
+<img src="h1.jpg" alt="photo" width="400" height="400">
+<b>C Programming(For 1st Sem)</b><br>
+<b>Price:</b><p>Rs.350</p>
+<a href="Select.php" name="1st" target="_blank">Add to cart</a><br>
+
+</div>
+<div class="gallery">
+<img src="h2.webp" alt="photo" width="400" height="400">
+<b>Python Programming(For 2nd Sem)</b><br>
+<b>Price:</b><p>Rs.410</p>
+<a href="OrdersDetailsform.php" target="_blank">Add to cart</a><br>
+
+</div>
+<div class="gallery">
+<img src="h3.webp" alt="photo" width="400" height="400">
+<b>Data Structures With C(For 2nd Sem)</b><br>
+<b>Price:</b><p>Rs.400</p>
+<a href="OrdersDetailsform.php" target="_blank">Add to cart</a><br>
+
+</div>
+<div class="gallery">
+<img src="h4.jpg" alt="photo" width="400" height="400">
+<b>Object Oriented Programming with C++(For 3rd sem)</b><br>
+<b>Price:</b><p>Rs.300</p>
+<a href="OrdersDetailsform.php" target="_blank">Add to cart</a><br>
+
+</div>
+
+<div class="gallery">
+<img src="osy.jpg" alt="photo" width="400" height="400">
+<b>Operating System(For 5th Sem CO)</b><br>
+<b>Price:</b><p>Rs.300</p>
+<a href="OrdersDetailsform.php" target="_blank">Add to cart</a><br>
+</div>
+<div class="gallery">
+<img src="h5.jpg" alt="photo" width="400" height="400">
+<b>Database Management System(For 3rd Sem)</b><br>
+<b>Price:</b><p>Rs.360</p>
+<a href="OrdersDetailsform.php" target="_blank">Add to cart</a><br>
+
+</div>
+<div class="gallery">
+<img src="h.jpg" alt="photo" width="400" height="400">
+<b>Advanced Data Structures in c(For 4th Sem )</b><br>
+<b>Price:</b><p>Rs.370</p>
+<a href="OrdersDetailsform.php" target="_blank">Add to cart</a><br>
+</div>
+<div class="gallery">
+<img src="haa.jpg" alt="photo" width="390" height="390">
+<b>Agile Software Engineering(For 4th Sem)</b><br>
+<b>Price:</b><p>Rs.200</p>
+<a href="OrdersDetailsform.php" target="_blank">Add to cart</a><br>
+
+</div>
+<div class="gallery">
+<img src="h8.jpg" alt="photo" width="400" height="400">
+<b>Web based Application Development with Django Python(For 4th Sem)</b><br>
+<b>Price:</b><p>Rs.410</p>
+<a href="OrdersDetailsform.php" target="_blank">Add to cart</a><br>
+
+</div>
+
+
+
+
+
+</body>
+</html>
